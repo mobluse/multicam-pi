16bc7488f216b7810983190bc7ead2504536aef2
Mikael Bonnier <mikael.bonnier@gmail.com>
2016-05-16 17:50:32 +0200

    Improved start.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo apt-get install vgrabbj
+sudo apt-get install libav-tools
 cp -n camera.sh ~
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sudo crontab cron.txt
+SCRIPT_PATH=$(dirname $(realpath -s $0))
+sudo crontab $SCRIPT_PATH/cron.txt
 sudo crontab -l
